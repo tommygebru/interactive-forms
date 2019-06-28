2b493425073ddbc5226851afe5d5fb7f4535cc4d
tommygebru <gebru.t@gmail.com>
2019-06-28 13:30:46 -0700

    more comments

--- js/script.js
+++ js/script.js
@@ -8,6 +8,74 @@ $("#title").change(event,()=>{
   if(event.target.value === "other"){
   $("#other-title").show();
 }//if
-});//click
+});//change
+
+//within Tshirt info
+//Design options determines the available Color options
+
+
+
+
+
+const newItem = document.createElement("option");
+newItem.setAttribute("placeholder", "choose a color");
+console.log(newItem);
+//const colorSelect = document.getElementsByTagName("#color option").prepend(newItem);
+$("#color").prepend(newItem);
+//console.log(colorSelect.previousElementSibling);
+//colorSelect.prev(newItem, colorSelect.childNodes[0]);
+
+
+
+
+
+
+
+//let newOp = document.createElement("option")
+
+//let setOp = newOp.setAttribute("placeholder", "choose a color");
+//console.log(setOp);
+//let firstOp = document.querySelector("#color option")[0].previousElementSibling
+//console.log(firstOp);
+//allOps[0]= setOp;
+
+
+
+
+
+
+
+//  $("#design option").firstChild(newOp);
+//designOp.firstElementChild(newOp);
+
+
+
+
+
+
+//if selecting js puns hide just tshirt, else show puns
+$("#design").change(event, ()=>{
+  console.log(event.target.value);
+
+if(event.target.value === "js puns"){
+$("option[value='tomato']").hide();
+$("option[value='steelblue']").hide();
+$("option[value='dimgrey']").hide();
+$("option[value='darkslategrey']").show();
+$("option[value='cornflowerblue']").show();
+$("option[value='gold']").show();
+  }
+  else if(event.target.value === "heart js"){
+$("option[value='darkslategrey']").hide();
+$("option[value='cornflowerblue']").hide();
+$("option[value='gold']").hide();
+$("option[value='tomato']").show();
+$("option[value='steelblue']").show();
+$("option[value='dimgrey']").show();
+}
+});//change
+
+
+
 
 });//document.ready
