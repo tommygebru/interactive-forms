102e7fe05d42cadfe1c26ffa0bd9166439595fcd
tommygebru <gebru.t@gmail.com>
2019-06-28 10:35:47 -0700

    the change method

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 //autofocus the first input element
 $("#name").focus();
-});
+$("#other-title").hide();
+
+$("#title").change(event,()=>{
+  if(event.target.value === "other"){
+  $("#other-title").show();
+}//if
+});//click
+
+});//document.ready
