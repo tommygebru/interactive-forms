db3185156af75f2a87f8742625e4c9dff0cf0d37
tommygebru <gebru.t@gmail.com>
2019-07-16 21:50:52 -0700

    tshirt section, update to the theme, hide all option initially

--- js/script.js
+++ js/script.js
@@ -75,6 +75,34 @@ $("#title").change(event, () => {
 **********************************************/
 
 
+//create a default option for the Color choices
+$("#color").prepend(`<option value="other" selected>Choose a color</option>`);
+//if selecting js puns hide just tshirt, else show puns
+
+//$("#color option:not(option[value='other'])").hide();
+$("#color option").hide();
+$("#color option:first").show();
+
+$("#design").change(event, () => {
+	if (event.target.value === "js puns") {
+		$("option[value='tomato']").hide();
+		$("option[value='steelblue']").hide();
+		$("option[value='dimgrey']").hide();
+		$("option[value='darkslategrey']").show();
+		$("option[value='cornflowerblue']").show();
+		$("option[value='gold']").show();
+	} else if (event.target.value === "heart js") {
+		$("option[value='darkslategrey']").hide();
+		$("option[value='cornflowerblue']").hide();
+		$("option[value='gold']").hide();
+		$("option[value='tomato']").show();
+		$("option[value='steelblue']").show();
+		$("option[value='dimgrey']").show();
+	}
+}); //change
+
+
+
 
 
 
