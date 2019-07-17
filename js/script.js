c62c81edaeaf8ccf82a5d996f80fae5a3564e8fe
tommygebru <gebru.t@gmail.com>
2019-07-16 20:58:43 -0700

    tidy code

--- js/script.js
+++ js/script.js
@@ -1,50 +1,65 @@
 $(document).ready(() => {
-
-const orange = "solid #ed723c 2px";
-const black = "solid black 2px";
-const blue = "solid #5e97b0 2px"
-
-const name = $("#name");
-const btn = $("button");
-
-
-btn.click(event,() => {
-	event.preventDefault();
-});
-const valid = () => {
-btn.css("border", black);
-btn.prop("disable", false);
-$(".review-form").remove();
-}
-const invalid = () => {
-	btn.css("border", orange);
-	btn.prop("disable", true);
-	$("form:last-child").append("<h4 class='review-form'> * Review The Form For Errors Before Submitting</h4>");
-	$(".review-form").css("color", "#ed723c");
-}
-
-
-
-name.focus();
-
-	if(name.val().length === 0){
+	/**********************************************
+		B A S E
+	**********************************************/
+	const orange = "solid #ed723c 2px";
+	const black = "solid black 2px";
+	const blue = "solid #5e97b0 2px";
+	const name = $("#name");
+	const btn = $("button");
+	btn.click(event, () => {
+		event.preventDefault();
+	});
+	const valid = () => {
+		btn.css("border", black);
+		btn.prop("disable", false);
+		$(".review-form").remove();
+	};
+	const invalid = () => {
+		btn.css("border", orange);
+		btn.prop("disable", true);
+		$("form:last-child").append("<h4 class='review-form'> * Review The Form For Errors Before Submitting</h4>");
+		$(".review-form").css("color", "#ed723c");
+	};
+	/**********************************************
+		N A M E
+	**********************************************//*
+	name.focus();
+	if (name.val().length === 0) {
 		console.log(1);
 		name.css("border", orange);
 		invalid();
 		console.log(1);
-	}//if
-	name.keyup(()=>{
-		if(name.val().length === 0){
+	} //if
+	name.keyup(() => {
+		if (name.val().length === 0) {
 			console.log(2);
 			name.css("border", orange);
 			invalid();
 			console.log(2);
-		}
-		else{
+		} else {
 			name.css("border", blue);
 			console.log(3);
 			valid();
 			console.log(3);
-		}//else
-	});//keyup
-});//document.ready
+		} //else
+	}); //keyup
+	*//**********************************************
+		E M A I L
+	**********************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //document.ready
