76c89c6db0c9e69fb15c3ab146ac5ffe97ad3e8c
tommygebru <gebru.t@gmail.com>
2019-07-16 22:45:44 -0700

    error validationand message for the activities section

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ $(document).ready(() => {
 	const name = $("#name");
 	const btn = $("button");
 	const email = $("#mail");
-
 	btn.click(event, () => {
 		event.preventDefault();
 	});
@@ -25,88 +24,115 @@ $(document).ready(() => {
 	};
 	/**********************************************
 		N A M E
-	**********************************************//*
-	name.focus();
-	if (name.val().length === 0) {
-		name.css("border", orange);
-		invalid();
-	} //if
-	name.keyup(() => {
+	**********************************************/
+	/*
+		name.focus();
 		if (name.val().length === 0) {
 			name.css("border", orange);
 			invalid();
-		} else {
-			name.css("border", blue);
-			valid();
-		} //else
-	}); //keyup
-	*//**********************************************
-		E M A I L
-	**********************************************//*
-	if (email.val().length === 0) {
-		email.css("border", orange);
-		invalid();
-	} //if
-	email.keyup(()=>{
-		let string = email.val();
-		let regexmail = /^([a-z]+\@[a-z]+\.com)$/i;
-		if (regexmail.test(string)){
-			email.css("border", blue);
-			valid();
-		} else {
+		} //if
+		name.keyup(() => {
+			if (name.val().length === 0) {
+				name.css("border", orange);
+				invalid();
+			} else {
+				name.css("border", blue);
+				valid();
+			} //else
+		}); //keyup
+		*/
+	/**********************************************
+			E M A I L
+		**********************************************/
+	/*
+		if (email.val().length === 0) {
 			email.css("border", orange);
 			invalid();
+		} //if
+		email.keyup(()=>{
+			let string = email.val();
+			let regexmail = /^([a-z]+\@[a-z]+\.com)$/i;
+			if (regexmail.test(string)){
+				email.css("border", blue);
+				valid();
+			} else {
+				email.css("border", orange);
+				invalid();
+			}
+		});
+	*/
+	/***********************************************
+		J O B    R O L E
+	**********************************************/
+	/*
+	$("#other-title").hide();
+	//upon changing the selection drop down, show text field
+	$("#title").change(event, () => {
+		if (event.target.value === "other") {
+			$("#other-title").show();
+		} else {
+			$("#other-title").hide();
 		}
-	});
-*//***********************************************
-	J O B    R O L E
-**********************************************//*
-$("#other-title").hide();
-//upon changing the selection drop down, show text field
-$("#title").change(event, () => {
-	if (event.target.value === "other") {
-		$("#other-title").show();
-	} else {
-		$("#other-title").hide();
+	}); //change
+	/***********************************************
+		T S H I R T S
+	**********************************************/
+	/*
+	//create a default option for the Color choices
+	$("#color").prepend(`<option value="other" selected>Choose a color</option>`);
+	//if selecting js puns hide just tshirt, else show puns
+
+	//$("#color option:not(option[value='other'])").hide();
+	$("#color option").hide();
+	$("#color option:first").show();
+
+	$("#design").change(event, () => {
+		if (event.target.value === "js puns") {
+			$("option[value='tomato']").hide();
+			$("option[value='steelblue']").hide();
+			$("option[value='dimgrey']").hide();
+			$("option[value='darkslategrey']").show();
+			$("option[value='cornflowerblue']").show();
+			$("option[value='gold']").show();
+		} else if (event.target.value === "heart js") {
+			$("option[value='darkslategrey']").hide();
+			$("option[value='cornflowerblue']").hide();
+			$("option[value='gold']").hide();
+			$("option[value='tomato']").show();
+			$("option[value='steelblue']").show();
+			$("option[value='dimgrey']").show();
+		}
+	}); //change
+	*/
+	/**********************************************
+		A C T I V I T I E S
+	**********************************************/
+	//Create a calculator for the total activity costs
+	let calcTotal = 0;
+	$(".activities").append(`<h4>TOTAL: </h4>`);
+	$(".activities input").change(event, () => {
+		let activity = event.target.parentNode.innerHTML;
+		let prices = activity.slice(-3);
+		if (event.target.checked == true) {
+			calcTotal += Number(prices);
+			$(".activities h4").html(`TOTAL: $${calcTotal}`);
+		} else {
+			calcTotal -= Number(prices);
+			$(".activities h4").html(`TOTAL $${calcTotal}`);
+		}
+	}); //change
+
+btn.click(()=>{
+	if(calcTotal === 0){
+		$(".activities").prepend(`<h3>* Choose at least one</h3>`);
+		$(".activities h3").css("color", "#ed723c");
+		invalid();
+	}else if(calcTotal !==0){
+		$(".activities h3").remove();
+		valid();
+
 	}
-}); //change
-/***********************************************
-	T S H I R T S
-**********************************************/
-
-
-//create a default option for the Color choices
-$("#color").prepend(`<option value="other" selected>Choose a color</option>`);
-//if selecting js puns hide just tshirt, else show puns
-
-//$("#color option:not(option[value='other'])").hide();
-$("#color option").hide();
-$("#color option:first").show();
-
-$("#design").change(event, () => {
-	if (event.target.value === "js puns") {
-		$("option[value='tomato']").hide();
-		$("option[value='steelblue']").hide();
-		$("option[value='dimgrey']").hide();
-		$("option[value='darkslategrey']").show();
-		$("option[value='cornflowerblue']").show();
-		$("option[value='gold']").show();
-	} else if (event.target.value === "heart js") {
-		$("option[value='darkslategrey']").hide();
-		$("option[value='cornflowerblue']").hide();
-		$("option[value='gold']").hide();
-		$("option[value='tomato']").show();
-		$("option[value='steelblue']").show();
-		$("option[value='dimgrey']").show();
-	}
-}); //change
-
-
-
-
-
-
-
+});
 
 
 
